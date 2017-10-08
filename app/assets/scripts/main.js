748cd8e6c1a5055bbced6687ddd1d6f77cedcbb3
David <dvdgsl@gmail.com>
2017-10-08 22:26:17 +0200

    added Bootstrap, a tweet button and refactored code

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,7 +1,59 @@
-$('#quote-button').on('click', function() {
-	var randomNumber = Math.floor(Math.random()* quotes.length);
-	var randomQuote = quotes[randomNumber];
-	$('#quote-area').text(randomQuote.Quote);
-	$('#author-area').text(randomQuote.Author);
+// (function(){
 
-});
+// var quotesGenerator = {
+	
+// 	init: function() {
+// 		this.events();
+// 	},
+	
+// 	cacheDom: function() {
+// 		this.$el = $('.quote-machine');
+// 		this.quoteButton = this.$el.find('#quote-button');
+// 		this.quoteArea = this.$el.find('#quote-area');
+// 		this.authorArea = this.$el.find('#author-area');
+// 	},
+
+// 	render: function() {
+// 		this.cacheDom();
+// 		var randomNumber = Math.floor(Math.random()* quotes.length);
+// 		var randomQuote = quotes[randomNumber];
+// 		this.quoteArea.text(randomQuote.Quote);
+// 		this.authorArea.text(randomQuote.Author);
+// 	},
+
+// 	events: function() {
+// 		console.log(this.render());
+// 		this.quoteButton.click(this.render());
+// 	}
+// };
+
+// quotesGenerator.init();
+// })()
+
+(function(){
+	var quoteMachine = $('#quote-machine');
+	var quoteButton = quoteMachine.find('#quote-button');
+	var tweetButton = quoteMachine.find('#tweet-button');
+	var quoteArea = quoteMachine.find('#quote-area');
+	var authorArea = quoteMachine.find('#author-area');
+	var randomQuote = '';
+
+	quoteButton.on('click', function() {
+	var randomNumber = Math.floor(Math.random()* quotes.length);
+	randomQuote = quotes[randomNumber];
+	quoteArea.text(randomQuote.Quote);
+	authorArea.text(randomQuote.Author);
+	tweetButton.removeClass('is-invisible');
+	});
+
+	tweetButton.on('click', function() {
+		if (randomQuote != '') {
+			tweetText = randomQuote.Quote + ' - ' + randomQuote.Author;
+			console.log(tweetText);
+			window.open('https://twitter.com/intent/tweet?text='+tweetText);
+		} else {
+			alert('Please generate a Quote!');
+		}
+	});
+
+})()
--- app/assets/scripts/quotes.js
+++ app/assets/scripts/quotes.js
@@ -12,6 +12,53 @@ var quotes = [
 	{
 		Author: "Edmund Burke",
 		Quote: "The only thing necessary for the triumph of evil is for good men to do nothing."
-	}
+	},
+
+	{
+		Author: "Winston Churchill1",
+		Quote: "If you're going through hell, keep going."
+	},
+
+	{
+		Author: "Ernest Hemingway2",
+		Quote: "But man is not made for defeat. A man can be destroyed but not defeated."
+	},
+
+	{
+		Author: "Edmund Burke3",
+		Quote: "The only thing necessary for the triumph of evil is for good men to do nothing."
+	},
+
+	{
+		Author: "Winston Churchill4",
+		Quote: "If you're going through hell, keep going."
+	},
+
+	{
+		Author: "Ernest Hemingway5",
+		Quote: "But man is not made for defeat. A man can be destroyed but not defeated."
+	},
+
+	{
+		Author: "Edmund Burke6",
+		Quote: "The only thing necessary for the triumph of evil is for good men to do nothing."
+	},
+
+	{
+		Author: "Winston Churchill7",
+		Quote: "If you're going through hell, keep going."
+	},
+
+	{
+		Author: "Ernest Hemingway8",
+		Quote: "But man is not made for defeat. A man can be destroyed but not defeated."
+	},
+
+	{
+		Author: "Edmund Burke9",
+		Quote: "The only thing necessary for the triumph of evil is for good men to do nothing."
+	},
+
+
 
 ];
